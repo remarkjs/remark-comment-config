0572671002b32f97082f7d9e78e7c04c1d111868
Titus Wormer <tituswormer@gmail.com>
2015-08-31 14:04:12 +0200

    Update eslint-env

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
+/**
+ * @author Titus Wormer
+ * @copyright 2015 Titus Wormer
+ * @license MIT
+ * @module mdast:comment-config
+ * @fileoverview Test suite for `mdast-comment-config`.
+ */
+
 'use strict';
 
-/* eslint-env mocha */
+/* eslint-env node, mocha */
 
 /*
  * Dependencies.
