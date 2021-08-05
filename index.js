1a47b7e661dc7b99fed1c6142a560e0f291a2466
Titus Wormer <tituswormer@gmail.com>
2021-08-05 12:11:52 +0200

    Remove warning for remark 12

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 import {commentMarker} from 'mdast-comment-marker'
 
-let warningIssued
-
 // Modify remark to read configuration from comments.
 export default function remarkCommentConfig() {
   const data = this.data()
-
-  // Old remark.
-  /* c8 ignore next 11 */
-  if (
-    !warningIssued &&
-    this.Compiler &&
-    this.Compiler.prototype &&
-    this.Compiler.prototype.visitors
-  ) {
-    warningIssued = true
-    console.warn(
-      '[remark-comment-config] Warning: please upgrade to remark 13 to use this plugin'
-    )
-  }
 
   // Other extensions.
   /* c8 ignore next */
